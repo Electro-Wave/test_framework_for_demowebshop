e5d5777c1f54432c6810c2ea14ba79fab90d37ed
kirill <->
2022-03-27 23:19:13 +0300

    Добавил тесты

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,7 +1,7 @@
 import allure
 import pytest
-from pages import MainPage
 from pages import CartPage
+from pages import MainPage
 
 
 @pytest.fixture
@@ -13,8 +13,8 @@ def add_item_to_cart(browser, url):
     page.click_to_cat()
 
 
-@allure.feature("Корзина")
-@allure.title("Добавить товав в корзину")
+@allure.feature('Корзина')
+@allure.title('Добавить товав в корзину')
 @pytest.mark.cart
 def test_add_product_cart(add_item_to_cart, browser):
     page = CartPage(browser)
@@ -22,8 +22,8 @@ def test_add_product_cart(add_item_to_cart, browser):
     page.amount_of_items_with_cart(1)
 
 
-@allure.feature("Корзина")
-@allure.title("Удаление товара из корзины")
+@allure.feature('Корзина')
+@allure.title('Удаление товара из корзины')
 @pytest.mark.cart
 def test_delete_item_in_cart(add_item_to_cart, browser):
     page = CartPage(browser)
@@ -31,3 +31,24 @@ def test_delete_item_in_cart(add_item_to_cart, browser):
     page.click_checkbox_remove()
     page.click_button_update_cart()
     page.should_be_empty_cart()
+
+
+@allure.feature('Корзина')
+@allure.title('Пересчет стоимости заказа при изменении количества товара в корзине')
+@pytest.mark.cart
+def test_recalculation_of_the_cost_of_the_order(add_item_to_cart, browser):
+    page = CartPage(browser)
+    page.should_be_form_order_summary()
+    page.input_field_qty(3)
+    page.click_button_update_cart()
+    page.check_change_price(30.0)
+
+
+@allure.feature('Корзина')
+@allure.title('Добавить товар в корзину и продолжить покупки')
+@pytest.mark.cart
+def test_add_item_to_cart_and_continue_shopping(add_item_to_cart, browser):
+    page = CartPage(browser)
+    page.should_be_form_order_summary()
+    page.click_button_continue_shopping()
+    page.current_url_should_be('http://demowebshop.tricentis.com/books')
